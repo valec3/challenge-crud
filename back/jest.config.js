3d54ce496f0003ea95338764ff125603c4f740bc
valec3 <v1c70rotakmer@gmail.com>
2025-04-13 23:25:46 -0500

    chore: initialize project with frontend and backend configurations

---
+++ back/jest.config.js
@@ -0,0 +1,13 @@
+export default {
+  testEnvironment: 'node',
+  verbose: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
+  transform: {},
+  extensionsToTreatAsEsm: ['.js'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  }
+};
---
+++ front/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button className="cursor-pointer">Click me</Button>
+    </div>
+  )
+}
+
+export default App
